766e1ab47174652a39fd4069bf4e0c674df0695b
biplob4 <biplobhossen6020@gmail.com>
2022-05-01 00:18:19 +0600

    adding fotter

--- src/Pages/Sheard/Footer/Footer.js
+++ src/Pages/Sheard/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import footerImg from '../../../icon/icon2-removebg-preview.png';
-import { faVoicemail} from '@fortawesome/free-solid-svg-icons';
 import './Fotter.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram,faGoogle, faGithub, faLinkedin, faYoutube, faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelopeCircleCheck, faPhone } from '@fortawesome/free-solid-svg-icons';
 
 const Footer = () => {
     return (
@@ -34,27 +35,30 @@ const Footer = () => {
                         <div class="col-md-3 col-xs-12">
                             <div class="first">
                                 <h4>Contuct</h4>
-                                <p> <FontAwesomeIcon icon={faVoicemail}></FontAwesomeIcon> Email : biplobhossen6020@gmail.com</p>
-                                <p> Facebook : https://www.facebook.com/mdbiplob.hossen.3323/</p>
-                                <p> Phone : 0 1 7 5 4 - 1 0 2 7 2 6</p>
-                                <p> Instragrme : https://www.instagram.com/mdbiplob462/</p>
+                                <p> <FontAwesomeIcon icon={faPhone} color='#09AEEC' size='xl'></FontAwesomeIcon> Phone : 0 1 7 5 4 - 1 0 2 7 2 6</p>
+                                <p> <FontAwesomeIcon icon={faEnvelopeCircleCheck} color='green' size="xl"></FontAwesomeIcon> Email : biplobhossen6020@gmail.com</p>
+                                <p> <FontAwesomeIcon icon={faFacebook} size="xl" color='#528BFF'/> Facebook : https://www.facebook.com/mdbiplob.hossen.3323/</p>
+                                <p> <FontAwesomeIcon icon={faInstagram} size="xl" color='#5F6D9E' /> Instragrme : https://www.instagram.com/mdbiplob462/</p>
                             </div>
                         </div>
 
                         <div class="col-md-3 col-xs-12">
-                            <div class="first">
+                            <div class="first socialbtns">
                                 <h4>Fllow Us</h4>
-                                <p> Analytical Skills</p>
-                                <p> Problem-solving skills</p>
-                                <p> Critical-thinking skills</p>
-                                <p> Detail-oriented</p>
-                                <p> Multitasking</p>
-                                <p> Self-motivated</p>
-                                <p> Self-motivated</p>
+                                <a href="https://mail.google.com/mail/u/0/#inbox"  rel="noopener noreferrer" target="_blank"><FontAwesomeIcon className='iconSocial' color='#DD4B39' icon={faGoogle} size='xxl'></FontAwesomeIcon></a>
+
+                                <a href="https://www.facebook.com/mdbiplob.hossen.3323/"  rel="noopener noreferrer" target="_blank"><FontAwesomeIcon className='iconSocial' color='#1877F2' icon={faFacebook} size='xxl'></FontAwesomeIcon></a>
+
+                                <a href="https://www.instagram.com/mdbiplob462/"  rel="noopener noreferrer" target="_blank"><FontAwesomeIcon className='iconSocial' color='#784CAC' icon={faInstagram} size='xxl'></FontAwesomeIcon></a>
+
+                                <a href="https://github.com/biplob4"  rel="noopener noreferrer" target="_blank"><FontAwesomeIcon className='iconSocial' color='#009AD5' icon={faGithub} size='xxl'></FontAwesomeIcon></a>
+
+                                <a href="http://www.google.com"  rel="noopener noreferrer" target="_blank"><FontAwesomeIcon className='iconSocial' color='#E4405F' icon={faYoutube} size='xxl'></FontAwesomeIcon></a>
+
                             </div>
                         </div>
 
-                       
+
 
                     </div>
                 </div>
@@ -67,7 +71,7 @@ const Footer = () => {
                                 <a href="https://github.com/WebDeveloperCodeRep" target="_blank"> <i class="fab fa-github fa-2x margin"></i></a>
                                 <a href="https://www.linkedin.com/in/andreea-mihaela-bunget-a4248812b/" target="_blank"> <i class="fab fa-linkedin fa-2x margin"></i></a>
                                 <a href="https://www.youtube.com/channel/UCX674BUbomzBCakbb75lhfA?view_as=subscriber" target="_blank"><i class="fab fa-youtube fa-2x margin" ></i></a> */}
-                                 <p class="copyright">Toys warehouse © 2022</p>
+                                <p class="copyright">Toys warehouse © 2022</p>
                             </div>
 
                         </div>
